c5d03ca2901acbb876919e88562986225c40faaf
Mayran Adriel <mayranalbuquerque@gmail.com>
2024-07-07 15:01:11 -0300

    add ex1 ordenacao

---
+++ src/main/java/dio/mayran/segundaSemana/Set/exercicio1/ConjuntoPalavrasUnicas.java
@@ -0,0 +1,2 @@
+package dio.mayran.segundaSemana.Set.exercicio1;public class ConjuntoPalavrasUnicas {
+}
---
+++ src/main/java/dio/mayran/segundaSemana/Set/ordenacao/exercicio1/Tarefa.java
@@ -0,0 +1,2 @@
+package dio.mayran.segundaSemana.Set.ordenacao.exercicio1;public class Tarefa {
+}
